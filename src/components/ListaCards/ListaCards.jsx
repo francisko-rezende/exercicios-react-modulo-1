d37df7c02e6dbea53e62587c69c245355a115e63
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-05 07:39:57 -0300

    termina "[M1S08] Ex 5 - Modificando ListaCards"

--- src/components/ListaCards/ListaCards.jsx
+++ src/components/ListaCards/ListaCards.jsx
@@ -1,18 +1,23 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
+import { useState } from "react";
 
-import { Card } from '@components';
+import { Card } from "@components";
 
-import styles from './ListaCards.module.css';
+import styles from "./ListaCards.module.css";
 
-export const ListaCards = ({ produtos }) => (
-  <ul className={styles.lista}>
-    {produtos.map((produto) => (
-      <li className={styles.listaItem} key={produto.id}>
-        <Card produto={produto} />
-      </li>
-    ))}
-  </ul>
-);
+export const ListaCards = ({ produtos }) => {
+  const [produtosSelecionados, setProdutosSelecionados] = useState([]);
+
+  return (
+    <ul className={styles.lista}>
+      {produtos.map((produto) => (
+        <li className={styles.listaItem} key={produto.id}>
+          <Card produto={produto} />
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 ListaCards.propTypes = {
   produtos: PropTypes.array.isRequired,
