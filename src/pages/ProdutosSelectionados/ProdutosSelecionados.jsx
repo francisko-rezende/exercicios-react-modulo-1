8a6446eff11cbb03fd59a177a88e00ff8e245fe1
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-07 09:53:53 -0300

    termina "[M1S09] Ex 9 - Adicionar total dos produtos selecionados"

--- src/pages/ProdutosSelectionados/ProdutosSelecionados.jsx
+++ src/pages/ProdutosSelectionados/ProdutosSelecionados.jsx
@@ -2,12 +2,18 @@ import { useProducts } from "../../hooks/useProducts";
 import React from "react";
 import { ListaCards } from "@components";
 
+const getValorTotal = (produtos) =>
+  produtos.reduce((acc, { valor }) => acc + valor, 0);
+
 export const ProdutosSelecionados = () => {
   const { produtosSelecionados } = useProducts();
+
+  const valorTotal = getValorTotal(produtosSelecionados);
 
   return (
     <>
       <h1>Produtos selecionados</h1>
+      <p>Valor total: {`R$ ${valorTotal.toFixed(2).replace(".", `,`)}`}</p>
       <ListaCards produtos={produtosSelecionados} />
     </>
   );
