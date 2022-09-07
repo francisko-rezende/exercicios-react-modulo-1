01d24239f381d36019c5cbace68884d8d90eebdb
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2022-09-07 09:38:50 -0300

    termina "[M1S09] Ex 7 - Criar novo item de menu"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,25 @@
-import { logo } from '@images';
+import { logo } from "@images";
+import { Link } from "react-router-dom";
 
-import styles from './Header.module.css';
+import styles from "./Header.module.css";
 export const Header = () => (
   <header className={styles.header}>
     <div className={styles.titleContainer}>
-      <img src={logo} alt='Logo do restaurante' height={50} />
+      <img src={logo} alt="Logo do restaurante" height={50} />
       <h1 className={styles.title}>Green Food</h1>
     </div>
 
     <nav>
       <ul className={styles.ul}>
         <li>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className={styles.link} href='#'>
+          <Link className={styles.link} to="/">
             Menu
-          </a>
+          </Link>
+        </li>
+        <li>
+          <Link className={styles.link} to="minhas-escolhas">
+            Minhas escolhas
+          </Link>
         </li>
       </ul>
     </nav>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,11 +1,13 @@
 import { Produtos } from "../pages/produtos";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import { ProdutosSelecionados } from "../pages/ProdutosSelectionados";
 
 export const Router = () => {
   return (
     <Routes>
       <Route element={<Produtos />} path="/" />
+      <Route element={<ProdutosSelecionados />} path="minhas-escolhas" />
     </Routes>
   );
 };
